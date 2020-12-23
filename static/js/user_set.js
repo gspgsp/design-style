2ec683cf5b589085f80f538fca8a8a66d29209ab
guoshipeng <2249875980@qq.com>
2020-12-23 18:04:25 +0800

    个人中心scss

--- static/js/user_set.js
+++ static/js/user_set.js
@@ -3,6 +3,49 @@
     letDomCenter('.email-bind-verify');
     $('.user-set-shadow').show();
   });
+
+  $('.bind-btn-n').click(function () {
+    $('.email-bind-verify').hide();
+    $('.user-set-shadow').hide();
+  });
+
+  $('.bind-htn-y').click(function () {
+    $('.email-bind-verify').hide();
+    letDomCenter('.email-bind-show');
+  });
+
+  $('.email-bind-close').click(function () {
+    $('.email-bind-verify').hide();
+    $('.email-bind-show').hide();
+    $('.user-set-shadow').hide();
+  });
+
+  $('.email-bind-y').click(function () {
+    //TODO::保存数据库操作
+
+    $('.email-bind-show').hide();
+    $('.user-set-shadow').hide();
+  });
+
+  $('.email-bind-n').click(function () {
+    $('.email-bind-show').hide();
+    $('.user-set-shadow').hide();
+  });
+
+  $('.password-change').click(function () {
+    letDomCenter('.password-c-wrap');
+    $('.user-set-shadow').show();
+  });
+
+  $('.password-c-y').click(function () {
+    //TODO::保存数据库操作
+    $('.password-c-wrap').hide();
+    $('.user-set-shadow').hide();
+  });
+  $('.password-c-close, .password-c-n').click(function () {
+    $('.password-c-wrap').hide();
+    $('.user-set-shadow').hide();
+  });
 })
 
 /**
