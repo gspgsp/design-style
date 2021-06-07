934283e01cf786db7b8e03dcf183893b31137d91
guoshipeng <2249875980@qq.com>
2021-06-07 14:54:07 +0800

    搜索样式

---
+++ static/js/search.js
@@ -0,0 +1,8 @@
+;$(function () {
+  if($('.search-box').length > 0){
+    $('.search-nav-icon').click(function () {
+      $('.search-h-ip').show()
+      $('.search-box-hi').show()
+    })
+  }
+})
